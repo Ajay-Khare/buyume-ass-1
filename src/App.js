4cf5c8fe39b1fb953c04b6d1d8e419ecffd4f40e
cmanager124 <cmanager124@gmail.com>
2022-11-04 13:26:45 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // all initial data from api
+  const [data, setdata] = useState([]);
+  const [tempData, setTempData] = useState([])
+
+  // fetching data from API
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      headers: {
+        "content-type": "application/json"
+      },
+      method: "GET"
+    })
+      .then(rawData => rawData.json())
+      .then(res => {
+        setdata(res)
+        setTempData(res)
+      })
+  }, []);
+
+  // data of search bar
+  const [search, setSearch] = useState('');
+
+  // state for storing data of search result
+  const [searchData, setSearchData] = useState([]);
+  const searchHandler = (e) => {
+    // console.log(search.length)
+    let arr = []
+    e.preventDefault();
+    data.map(ele => {
+      if (search.split(" ").length == 1) {
+        ele.title.split(" ").map(newEle => {
+          if (newEle === search) {
+            arr.push(ele)
+          }
+        })
+      }
+      else {
+        if (ele.title.includes(search)) {
+          arr.push(ele)
+        }
+      }
+    })
+
+    setTempData(arr)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchContainer">
+        <input type="text"
+          placeholder='search title'
+          value={search} onChange={(e) => {
+            setSearch(e.target.value);
+
+          }} />
+        <br />
+        <button onClick={searchHandler}>Search</button>
+      </div>
+      <div className="dataContainer">
+        {tempData.map((ele, i) => {
+          return (
+            <>
+              <div className="card" id={ele.id}>
+                <span>Title : </span>
+                <span className='title' style={{fontWeight:"bold"}}>{ele.title}</span>
+                <p className='body'>Body : {ele.body}</p>
+              </div>
+              <hr />
+            </>
+
+          )
+        })}
+      </div>
+
+
     </div>
   );
 }
